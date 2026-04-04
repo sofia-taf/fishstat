37f45a4e9e800412cfcf34d64fbad4928712c5e3
Arni Magnusson <thisisarni@gmail.com>
2026-04-04 21:49:41 +1100

    Update row counts

--- R/capture.R
+++ R/capture.R
@@ -24,7 +24,7 @@
 #' }
 #'
 #' @details
-#' This data frame contains the full set of 1,055,015 data records from the
+#' This data frame contains the full set of 1,052,197 data records from the
 #' FishStat \emph{Capture Quantity} data table. Column names have been
 #' simplified to facilitate quick exploration and plotting in R.
 #'
